df526531ce0ac339afba729d9c1e81aed8cdeacf
wojole <olejarz.wojciech@gmail.com>
2017-06-30 13:40:25 +0200

    code align

--- src/User.php
+++ src/User.php
@@ -1,67 +1,79 @@
 <?php
 
-class User {
+class User
+{
 
     private $id;
     private $username;
     private $hashPass;
     private $email;
 
-    function __construct() {
+    function __construct()
+    {
         $this->id = -1;
         $this->username = "";
         $this->hashPass = "";
         $this->email = "";
     }
 
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getUsername() {
+    public function getUsername()
+    {
         return $this->username;
     }
 
-    public function getHashPass() {
+    public function getHashPass()
+    {
         return $this->hashPass;
     }
 
-    public function getEmail() {
+    public function getEmail()
+    {
         return $this->email;
     }
 
-    public function setUsername($username) {
+    public function setUsername($username)
+    {
         $this->username = $username;
     }
 
-    public function setPassword($newPass) {
+    public function setPassword($newPass)
+    {
 
         $newHashedPassword = password_hash($newPass, PASSWORD_BCRYPT);
         $this->hashPass = $newHashedPassword;
     }
 
-    public function setEmail($email) {
+    public function setEmail($email)
+    {
         $this->email = $email;
     }
 
-    public function saveToDB(PDO $conn) {
+    public function saveToDB(PDO $conn)
+    {
         if ($this->id == -1) {
             $stmt = $conn->prepare(
-                    'INSERT INTO Users(username, email, hash_pass) VALUES (:username, :email, :pass)'
+                'INSERT INTO Users(username, email, hash_pass) VALUES (:username, :email, :pass)'
             );
             $result = $stmt->execute(
-                    ['username' => $this->username, 'email' => $this->email, 'pass' => $this->hashPass]
+                ['username' => $this->username,
+                    'email' => $this->email,
+                    'pass' => $this->hashPass]
             );
             if ($result !== false) {
                 $this->id = $conn->lastInsertId();
                 return true;
-            }else echo "Błąd!";
+            } else echo "Błąd!";
         } else {//jeżeli id już istnieje, zaktualizuj rekord
             $stmt = $conn->prepare(
-                    'UPDATE Users SET username=:username, email=:email, hash_pass=:hash_pass WHERE id=:id'
+                'UPDATE Users SET username=:username, email=:email, hash_pass=:hash_pass WHERE id=:id'
             );
             $result = $stmt->execute(
-                    ['username' => $this->username, 'email' => $this->email, 'hash_pass' => $this->hashPass, 'id' => $this->id]
+                ['username' => $this->username, 'email' => $this->email, 'hash_pass' => $this->hashPass, 'id' => $this->id]
             );
             if ($result === true) {
                 return true;
@@ -72,7 +84,8 @@ class User {
     }
 
 //metoda wczytująca użytkownika po id:
-    static public function loadUserById(PDO $conn, $id) {
+    static public function loadUserById(PDO $conn, $id)
+    {
         $stmt = $conn->prepare('SELECT * FROM Users WHERE id=:id');
         $result = $stmt->execute(['id' => $id]);
 
@@ -91,7 +104,8 @@ class User {
     }
 
 //metoda wczytująca wszystkich użytkowników z bazy danych
-    static public function loadAllUsers(PDO $conn) {
+    static public function loadAllUsers(PDO $conn)
+    {
         $sql = "SELECT * FROM Users";
         $ret = [];
 
@@ -110,7 +124,8 @@ class User {
         return $ret; //metoda zwraca tablicę z obiektami
     }
 
-    public function delete(PDO $conn) {
+    public function delete(PDO $conn)
+    {
         if ($this->id != -1) {
             $stmt = $conn->prepare('DELETE FROM Users WHERE id=:id');
             $result = $stmt->execute(['id' => $this->id]);
@@ -124,8 +139,9 @@ class User {
         }
         return true;
     }
-    
-    static public function loadUserByEmail(PDO $conn, $email) {
+
+    static public function loadUserByEmail(PDO $conn, $email)
+    {
         $stmt = $conn->prepare('SELECT * FROM Users WHERE email=:email');
         $result = $stmt->execute(['email' => $email]);
 
